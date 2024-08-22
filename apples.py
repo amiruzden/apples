b84963b9c2111e6dbdd032b12f6a165c61cf3d47
Amir <a.uzdenov@pos-credit.ru>
2024-08-22 18:44:07 +0300

    Update apples.py

--- apples.py
+++ apples.py
@@ -3,11 +3,18 @@ def distribute_apples():
     n, m = map(int, input().split())
     
     # рассчитываем количество яблок у каждого человека
-    apples_per_person = n / m
+    apples_per_person = n // m
     remaining_apples = n % m
     
     # выводим результат в целыми яблоками
     print(f"Каждому достанется по {apples_per_person} яблок, {remaining_apples} яблок(о) осталось.")
+    
+    # расчитываем, если яблоки могут делиться на половинки
+    apples_per_person = n / m
+    remaining_apples = n % m
+    
+    # выводим результат с делением на половинки
+    print(f"Если яблоки делятся на половинки, каждому достанется по {apples_per_person:.1f} яблок, {remaining_apples:.1f} яблок (о) осталось.")
     
 if __name__ == "__main__":
     distribute_apples() 
